9e40467b985edd975abd8a88d20ec745d419458c
Mahlatse03 <mahlatsed@gmail.com>
2026-08-11 19:46:01 +0200

    Added dependencies and created framework, tests

---
+++ src/test/java/payloadBuilder/UserPayload.java
@@ -0,0 +1,40 @@
+package payloadBuilder;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class UserPayload {
+
+    public static JSONObject userRegistrationPayload(String firstName, String lastName, String email, String password, String groupId) {
+        JSONObject registerUser = new JSONObject();
+        registerUser.put("firstName", firstName); //adding key-value pairs in the registerUser object
+        registerUser.put("lastName", lastName);
+        registerUser.put("email", email);
+        registerUser.put("password", password);
+        registerUser.put("confirmPassword", password);
+        registerUser.put("groupId", groupId);
+
+        return registerUser;
+    }
+
+//    --commented out the example payload method for now, can be used for practice purposes later
+//    @Test
+//    public void payloadExample() {
+//        JSONObject examplePayload = new JSONObject();
+//        examplePayload.put("key1", "value1");
+//        examplePayload.put("key2", "value2");
+//
+//        System.out.println(examplePayload.toJSONString());
+//    }
+
+
+    public static JSONObject loginPayload(String email, String password) {
+        JSONObject loginUser = new JSONObject(); //instantiate loginUser object of type JSONObject
+        loginUser.put("email", email); //putting key-value pairs in the loginUser object
+        loginUser.put("password", password);
+
+        return loginUser;
+    }
+
+
+}
---
+++ src/test/java/requestBuilder/UserRequestBuilder.java
@@ -0,0 +1,50 @@
+package requestBuilder;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import payloadBuilder.UserPayload;
+
+import static commons.Routes.BASE_URL;
+import static io.restassured.RestAssured.given;
+
+public class UserRequestBuilder {
+
+    static String registeredUserId;
+    static String userToken;
+
+    public static Response userRegistrationRequest(String firstName, String lastName, String email, String password, String groupId) {
+
+        String apiPath = "/APIDEV/register";
+        Response response = given()
+                    .baseUri(BASE_URL)
+                    .basePath(apiPath)
+                    .contentType(ContentType.JSON)
+                    .body(UserPayload.userRegistrationPayload(firstName, lastName, email, password, groupId))
+                .when() //optional, can be removed
+                    .post()
+                .then()
+                    .extract().response();
+
+        registeredUserId = response.jsonPath().getString("data.id");
+        return response;
+    }
+
+    public static Response userLogin(String email, String password) {
+
+        String apiPath = "APIDEV/login";
+        Response response = given()
+                    .baseUri(BASE_URL)
+                    .basePath(apiPath)
+                    .header("Content-Type", "application/json") // similar to contentType(ContentType.JSON), just specific on the content type header
+                    .body(UserPayload.loginPayload(email, password))
+                .when()
+                    .post()
+                .then()
+                    .extract().response();
+
+        userToken = response.jsonPath().getString("data.token");
+        return response;
+    }
+
+
+}
